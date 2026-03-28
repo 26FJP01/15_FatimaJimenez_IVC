67515246bb6af92307c82b09b74fd08a2dc9d247
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 16:21:44 -0600

    Add files via upload

---
+++ Acctividad2_leccion20_incs1.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // VARIABLES
+        string contraseña;
+        // ENTRADA DE DATOS
+        Console.Write("Por favor ingrese la contraseña: ");
+        contraseña = Console.ReadLine();
+        // PROCESO Y VALIDACIÓN
+        if (contraseña == "Password123")
+        {
+            Console.WriteLine("Bienvenido");
+        }
+        else
+        {
+            Console.WriteLine("Ingreso prohibido");
+        }
+    }
+}
---
+++ Acctividad2_leccion20_incs1.psc
@@ -0,0 +1,10 @@
+Algoritmo Acctividad2_leccion20_incs1
+		Definir contraseña Como Cadena
+		Escribir "Por favor ingrese la contraseña:"
+		Leer contraseña
+		Si contraseña = "Password123" Entonces
+			Escribir "Bienvenido"
+		Sino
+			Escribir "Ingreso prohibido"
+		Fin Si
+FinAlgoritmo
