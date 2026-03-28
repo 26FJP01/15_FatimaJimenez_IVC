d20d04209bf7feb509a14eb7564f748ef1ace5b4
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 17:01:04 -0600

    Add files via upload

---
+++ Actividad1_leccion21_incs1CS.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numeroDia;
+        Console.Write("Ingrese un numero del 1 al 7: ");
+        numeroDia = int.Parse(Console.ReadLine());
+        if (numeroDia == 1)
+            Console.WriteLine("Hoy es Lunes");
+        else if (numeroDia == 2)
+            Console.WriteLine("Hoy es Martes");
+        else if (numeroDia == 3)
+            Console.WriteLine("Hoy es Miercoles");
+        else if (numeroDia == 4)
+            Console.WriteLine("Hoy es Jueves");
+        else if (numeroDia == 5)
+            Console.WriteLine("Hoy es Viernes");
+        else if (numeroDia == 6)
+            Console.WriteLine("Hoy es Sabado");
+        else if (numeroDia == 7)
+            Console.WriteLine("Hoy es Domingo");
+        else
+            Console.WriteLine("Numero invalido");
+    }
+}
---
+++ Actividad1_leccion21_incs1PS.psc
@@ -0,0 +1,22 @@
+Algoritmo Actividad1_leccion21_incs1
+	Definir Dia Como Entero
+    Escribir Sin Saltar "Ingrese un numero del 1 al 7: "
+    Leer Dia
+    Segun Dia Hacer
+        1:
+            Escribir "Hoy es Lunes"
+        2:
+            Escribir "Hoy es Martes"
+        3:
+            Escribir "Hoy es Miercoles"
+        4:
+            Escribir "Hoy es Jueves"
+        5:
+            Escribir "Hoy es Viernes"
+        6:
+            Escribir "Hoy es Sabado"
+        7:
+            Escribir "Hoy es Domingo"
+        De Otro Modo:
+            Escribir "Numero invalido" 
+FinAlgoritmo
