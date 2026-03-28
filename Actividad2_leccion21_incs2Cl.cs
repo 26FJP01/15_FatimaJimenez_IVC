da78573d9edd8773f1b997de75e7e8cc008460c2
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 17:25:50 -0600

    Add files via upload

---
+++ Actividad2_leccion21_incs2Cl.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // DECLARACIÓN DE VARIABLES
+        int opcion;
+        // ENTRADA DE DATOS
+        Console.WriteLine("== BUZÓN DE VOZ ==");
+        Console.WriteLine("Seleccione una opción:");
+        Console.WriteLine("1. Ventas");
+        Console.WriteLine("2. Recepción");
+        Console.WriteLine("3. Dirección");
+        Console.WriteLine("4. Compras");
+        opcion = Convert.ToInt32(Console.ReadLine());
+        // PROCESO Y SALIDA
+        if (opcion == 1)
+        {
+            Console.WriteLine("Bienvenido al departamento de Ventas.");
+            Console.WriteLine("Responsable:  Diego Gonzalez\"");
+            Console.WriteLine("Correo: Diego.Gonzalez@empresa.gt.com");
+        }
+        else if (opcion == 2)
+        {
+            Console.WriteLine("Bienvenido al departamento de Recepción.");
+            Console.WriteLine("Responsable: Sofia Diaz");
+            Console.WriteLine("Correo: Sofia.Diaz@empresa.gt.com");
+        }
+        else if (opcion == 3)
+        {
+            Console.WriteLine("Bienvenido al departamento de Dirección.");
+            Console.WriteLine("Responsable: Carlos López");
+            Console.WriteLine("Correo: carlos.Lopez@empresa.gt.com");
+        }
+        else if (opcion == 4)
+        {
+            Console.WriteLine("Bienvenido al departamento de Compras.");
+            Console.WriteLine("Responsable: Gabriela Martínez");
+            Console.WriteLine("Correo: Gabriela.Martinez@empresa.gt.com");
+        }
+        else
+        {
+            Console.WriteLine("Opción no válida. Por favor, elija un número entre 1 y 4.");
+        }
+    }
+}
---
+++ Actividad2_leccion21_incs2PS.psc
@@ -0,0 +1,30 @@
+Algoritmo Actividad2_leccion21_incs2.psc
+		Definir opcion Como Entero
+		Escribir "== BUZÓN DE VOZ =="
+		Escribir "Seleccione una opción:"
+		Escribir "1. Ventas"
+		Escribir "2. Recepción"
+		Escribir "3. Dirección"
+		Escribir "4. Compras"
+		Leer opcion
+		Segun opcion Hacer
+			 1:
+				Escribir "Bienvenido al departamento de Ventas."
+				Escribir "Responsable: Diego Gonzalez"
+				Escribir "Correo: Diego.Gonzalez@empresa.gt.com"
+			 2:
+				Escribir "Bienvenido al departamento de Recepción."
+				Escribir "Responsable: Sofia Diaz"
+				Escribir "Correo: Sofia.Diaz@empresa.gt.com"
+			3:
+				Escribir "Bienvenido al departamento de Dirección."
+				Escribir "Responsable: Sofia Diaz"
+				Escribir "Correo: carlos.lopez@empresa.gt.com"
+			 4:
+				Escribir "Bienvenido al departamento de Compras."
+				Escribir "Responsable: Gabriela Martínez"
+				Escribir "Correo: Gabriela.martinez@empresa.gt.com"
+			De Otro Modo:
+				Escribir "Opción no válida. Por favor, elija un número entre 1 y 4."
+		FinSegun
+FinAlgoritmo
