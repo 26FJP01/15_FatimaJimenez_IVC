4834d2b2150319194eb052e5b93968169b3394d3
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-27 18:27:57 -0600

    Add files via upload

---
+++ Act1_Lecc7cl.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //declaracion de variables 
+        int edad;
+        Console.WriteLine("Ingrese su edad:");
+        edad=int.Parse(Console.ReadLine());
+        //proceso y salida
+        if (edad >= 18)
+            Console.WriteLine("Es mayor de edad:");
+        else
+            Console.WriteLine("Es menor de edad:");
+    }
+}
