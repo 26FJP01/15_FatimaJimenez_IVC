8e4b80edc9335c067877b333947da76e5dfc1c46
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 15:21:38 -0600

    Add files via upload

---
+++ Actividad2_leccion7_incs3.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ==MENSAJE DE BIENVENIDA==
+        Console.WriteLine("SELECCION DE MES");
+        // DECLARACION DE VARIABLES
+        int mes;
+        // ENTRADA DE DATOS
+        Console.WriteLine("Ingrese el número correspondiente al mes (1-12):");
+        mes = Convert.ToInt32(Console.ReadLine());
+        // PROCESO Y SALIDA
+        if (mes == 1)
+            Console.WriteLine("Enero");
+        else if (mes == 2)
+            Console.WriteLine("Febrero");
+        else if (mes == 3)
+            Console.WriteLine("Marzo");
+        else if (mes == 4)
+            Console.WriteLine("Abril");
+        else if (mes == 5)
+            Console.WriteLine("Mayo");
+        else if (mes == 6)
+            Console.WriteLine("Junio");
+        else if (mes == 7)
+            Console.WriteLine("Julio");
+        else if (mes == 8)
+            Console.WriteLine("Agosto");
+        else if (mes == 9)
+            Console.WriteLine("Septiembre");
+        else if (mes == 10)
+            Console.WriteLine("Octubre");
+        else if (mes == 11)
+            Console.WriteLine("Noviembre");
+        else if (mes == 12)
+            Console.WriteLine("Diciembre");
+        else
+            Console.WriteLine("Opción no válida, por favor ingrese un número entre 1 y 12.");
+    }
+}
+ 
---
+++ Actividad2_leccion7_incs3ps.psc
@@ -0,0 +1,33 @@
+Algoritmo Actividad2_leccion7_incs3
+	Definir mes Como Entero
+	Escribir "Por favor, ingrese el número correspondiente al mes: "
+	Leer mes
+	Segun mes Hacer
+		1:
+			Escribir "Enero"
+		2:
+			Escribir "Febrero"
+		3:
+			Escribir "Marzo"
+		4:
+			Escribir "Abril"
+		5:
+			Escribir "Mayo"
+		6:
+			Escribir "Junio"
+		7:
+			Escribir "Julio"
+		8:
+			Escribir "Agosto"
+		9:
+			Escribir "Septiembre"
+		10:
+			Escribir "Octubre"
+		11:
+			Escribir "Noviembre"
+		12:
+			Escribir "Diciembre"
+		De Otro Modo:
+			Escribir "Número de mes no  es válido, por favor ingrese un número entre 1 a 12."
+	FinSegun
+FinAlgoritmo
