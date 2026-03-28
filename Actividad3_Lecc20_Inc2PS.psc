f65b9e280bfb5625235b97ea69ac182b3902b2ae
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 16:42:33 -0600

    Add files via upload

---
+++ Actividad3_Lecc20_Inc2PS.psc
@@ -0,0 +1,10 @@
+Algoritmo Actividad3_leccion20_incs2
+	Definir calificacion Como Entero
+    Escribir Sin Saltar "Ingrese nota: "
+    Leer calificacion
+    Si calificacion >= 70 Entonces
+        Escribir "Aprobado"
+    Sino
+        Escribir "Reprobado"
+    FinSi
+FinAlgoritmo
---
+++ Actividad3_leccion20_incs2CL.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int calificacion;
+        Console.Write("Ingrese nota: ");
+        calificacion = int.Parse(Console.ReadLine());
+        if (calificacion >= 70)
+            Console.WriteLine("Aprobado");
+        else
+            Console.WriteLine("Reprobado");
+    }
+}
