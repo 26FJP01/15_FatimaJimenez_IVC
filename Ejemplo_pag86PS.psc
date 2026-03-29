e49bcb0d803a4f1db7704ed337588df49759322a
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-28 23:04:36 -0600

    Add files via upload

---
+++ Ejemplo_pag86PS.psc
@@ -0,0 +1,10 @@
+Algoritmo Ejemplo_pag86
+Definir numero Como Entero
+Escribir "Introduzca un número:"
+Leer numero
+Si numero < 0 Entonces
+	Escribir "El número es menor a cero"
+Sino
+	Escribir "El número es igual o mayor a cero"
+FinSi
+FinAlgoritmo
---
+++ Pag-86CS.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero;
+        Console.WriteLine("Introduzca un número:");
+        numero = int.Parse(Console.ReadLine());
+        if (numero < 0) { 
+            Console.WriteLine("El número es menor a cero");
+        }
+        else
+            Console.WriteLine("El número es igual o mayor a cero");
+    }
+}
