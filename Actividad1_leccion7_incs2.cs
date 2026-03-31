65c12725d0dc5bb92bc743bb6092b3588706ecfb
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-03-31 12:54:59 -0600

    Add files via upload

---
+++ Actividad1_leccion7_incs2.cs
@@ -0,0 +1,10 @@
+Algoritmo sin_titulo
+	Definir edad Como Entero
+    Escribir Sin Saltar "Ingrese su edad: "
+    Leer edad
+    Si edad >= 18 Entonces
+        Escribir "Ingres su edad si es mayor de edad"
+    Sino
+        Escribir "Ingres su edad si es menor de edad"
+    FinSi
+FinAlgoritmo
